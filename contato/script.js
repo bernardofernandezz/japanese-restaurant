46583b7014a90169b109cdcae65cd6c68cdfb53e
Bernardo Fernandez Teixeira <bernafernandez@x@gmail.com>
2024-07-05 10:25:50 -0300

    feat: add footer on contact page

--- contato/script.js
+++ contato/script.js
@@ -12,7 +12,6 @@ document
       // Simulate form submission
       setTimeout(function () {
         document.getElementById("successMessage").classList.remove("hidden");
-        document.getElementById("contactForm").reset();
       }, 1000);
     } else {
       document.getElementById("successMessage").classList.add("hidden");
@@ -20,40 +19,10 @@ document
     }
   });
 
-document
-  .getElementById("contactForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var message = document.getElementById("message").value;
-
-    if (name && email && message) {
-      document.getElementById("modalMessage").textContent =
-        document.getElementById("successMessage").textContent;
-      document.getElementById("successModal").style.display = "block";
-      // Clear the form fields
-      document.getElementById("contactForm").reset();
-    }
-  });
-
-// Script para fechar o modal
-var modal = document.getElementById("successModal");
-var span = document.getElementsByClassName("close")[0];
-
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
 // Mostrar popup ao enviar formulário
 function showCustomAlert() {
   document.getElementById("custom-alert").classList.remove("hidden");
+  document.getElementById("contactForm").reset();
 }
 // Fechar popup do formulário
 function closeCustomAlert() {
